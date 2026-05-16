5cdbc1afa71ac86696e27200150a96295d891e07
Edehbasil <basiledeh@gmail.com>
2026-05-16 11:15:13 +0100

    security: move secret key to environment variable

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from app.db.base import Base
 from app.db.session import engine
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
 from jose import jwt
+import os
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = "changethisbeforeproduction"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv("SECRET_KEY", "changethisbeforeproduction")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 def hash_password(password: str) -> str:
